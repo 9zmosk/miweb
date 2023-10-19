28c1bebd34562284e0ce0fe903f3a044d602ec15
Camilo Mora <sebaarimami@gmail.com>
2023-10-19 00:12:58 -0300

    Primer modulo de html teminadogit add .

---
+++ code/cursoHTML/1queEsHtml.js
@@ -0,0 +1,17 @@
+document.getElementById('quizForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe
+
+    const respuestaUsuario = document.querySelector('input[name="respuesta"]:checked');
+    const resultado = document.getElementById('resultado');
+
+    if (respuestaUsuario) {
+        if (respuestaUsuario.value === 'html') {
+            alert("¡Respuesta correcta!");
+            resultado.textContent = "Bien hecho, puedes avanzar al siguiente modulo"
+        } else {
+            alert("Respuesta incorrecta. Inténtalo de nuevo.");
+        }
+    } else {
+        alert("Por favor, selecciona una respuesta.");
+    }
+});
